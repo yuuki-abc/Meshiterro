59c49314cd12efbcdb4eb6798e392cf4596520df
yuuki-abc <yuki.abc.300@gmail.com>
2021-02-02 09:43:47 +0000

    [Update] 4-5 マイページビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
